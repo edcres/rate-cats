641e19091a879d19b0f759df3f26041253eb1a7a
Ed Cres <crespoed07@gmail.com>
2022-06-03 08:28:24 -0400

    Set up app navigation.

--- app/src/main/java/com/example/ratecats/MainActivity.kt
+++ app/src/main/java/com/example/ratecats/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.ratecats
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+
+/**
+ * tabs:
+ * - favorites
+ * - categories
+ * - gifs
+ * - all
+ */
+
+/** Maybe future:
+ * - Upload feature and upload tab.
+ */
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.navigationBarColor = ContextCompat.getColor(this, R.color.black)
+        }
     }
 }
